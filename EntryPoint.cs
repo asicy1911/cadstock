322e933922772a327bad9989fe222e1d69a6d739
asicy1911 <asicy1986@gmail.com>
2026-01-26 12:51:00 +0800

    Add ToolbarCommands assembly attribute

--- EntryPoint.cs
+++ EntryPoint.cs
@@ -1,6 +1,7 @@
 using Autodesk.AutoCAD.Runtime;
 
 [assembly: CommandClass(typeof(cadstockv2.Commands))]
+[assembly: CommandClass(typeof(cadstockv2.ToolbarCommands))]
 [assembly: CommandClass(typeof(cadstockv2.DropdownCommands))]
 [assembly: ExtensionApplication(typeof(cadstockv2.EntryPoint))]
 
@@ -10,7 +11,6 @@ namespace cadstockv2
     {
         public void Initialize()
         {
-            // ✅ 延迟到 Idle 再装，经典界面更稳
             ClassicToolbarDropdownStocks.InstallDeferred();
         }
 
