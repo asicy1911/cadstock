50131eed31da7843e62172c430760bcfcb6c797d
asicy1911 <asicy1986@gmail.com>
2026-01-26 10:58:09 +0800

    Update EntryPoint.cs

--- EntryPoint.cs
+++ EntryPoint.cs
@@ -1,13 +1,17 @@
 using Autodesk.AutoCAD.Runtime;
 
-[assembly: CommandClass(typeof(cadstock.Commands))]
-[assembly: ExtensionApplication(typeof(cadstock.EntryPoint))]
+[assembly: CommandClass(typeof(cadstockv2.Commands))]
+[assembly: ExtensionApplication(typeof(cadstockv2.EntryPoint))]
 
-namespace cadstock
+namespace cadstockv2
 {
     public class EntryPoint : IExtensionApplication
     {
-        public void Initialize() { }
+        public void Initialize()
+        {
+            // ✅ 经典界面：创建菜单栏下拉
+            ClassicMenu.TryInstall();
+        }
 
         public void Terminate()
         {
